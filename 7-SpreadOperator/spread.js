05e0864870f585cede704a6a4cc039971aa39625
devbratraghuvanshi <devbratcs0017@gmail.com>
2017-05-21 22:22:52 +0530

    7-SpreadOperator was missing

--- 7-SpreadOperator/spread.js
+++ 7-SpreadOperator/spread.js
@@ -1,2 +1,29 @@
 
-// spread  allows you to take an array and spread it out into its individual items
+// spreads  allows you to take an array and spread it out into its individual items
+
+console.log([1,2,3,4]) // it will log [1,2,3,4]
+
+console.log(...[1,2,3,4]) // with spread operator (...) it will log 1 2 3 4
+
+
+let first = [1,2,3,4];
+let second = [5,6,7,8];
+
+first.push(second);
+
+console.log(first); // it will log [1,2,3,4,[5,6,7,8]]// 
+
+//===================
+
+let first = [1,2,3,4];
+let second = [5,6,7,8];
+
+first.push(...second); 
+console.log(first); // it will log [1,2,3,4,5,6,7,8]// 
+
+
+let first = [1,2,3,4];
+function add(a,b,c,d) {
+    console.log(a+b+c+d);
+}
+add(...first);
